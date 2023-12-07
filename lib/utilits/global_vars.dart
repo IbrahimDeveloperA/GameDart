91b60a69575f078ae6723543b71c9512df8bbb7b
king <ibrahim.kasymov@gmail.com>
2023-12-07 12:17:50 +0600

    Initial commit

---
+++ lib/utilits/global_vars.dart
@@ -0,0 +1,8 @@
+import 'package:untitled/scenes/app_scene.dart';
+import 'package:untitled/scenes/game_scene.dart';
+
+class GlobalVars{
+  static AppScene currentScene = GameScene();
+  static double screenWidth = 0;
+  static double screenHeight = 0;
+}
